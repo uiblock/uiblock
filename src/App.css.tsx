f3735c79ac1f4dfc93169101bbf6cbbe7994e6e4
Wael Showair <waelshowair@duck.com>
2023-08-13 07:35:17 -0400

    feat: add initial setup (#5)
    ts, react, vite, vanilla-extract, storybook & vitest

---
+++ src/App.css.tsx
@@ -0,0 +1,6 @@
+import { globalStyle } from '@vanilla-extract/css'
+
+globalStyle('body', {
+  background: 'black',
+  color: 'white',
+})
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to UI Blocks</h1>
+    </>
+  )
+}
+
+export default App
