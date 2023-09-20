8095475e2994290b24e0086b2090b6a61e8ee213
Wael Showair <waelshowair@duck.com>
2023-09-20 13:35:03 -0400

    change error icon from circle to traingle

--- src/components/icons/error.tsx
+++ src/components/icons/error.tsx
@@ -1,4 +1,4 @@
-import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 import { Icon, IconProps } from './icon'
 
-export const ErrorIcon = (props: IconProps) => <Icon {...props} icon={faCircleExclamation} />
+export const ErrorIcon = (props: IconProps) => <Icon {...props} icon={faTriangleExclamation} />
